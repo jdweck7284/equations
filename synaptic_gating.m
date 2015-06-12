c560d30b25c346cb1099823160be4c302c6940f8
jdweck7284 <jonathandweck@gmail.com>
2015-06-12 15:05:01 -0400

    new file
    new edition

--- synaptic_gating.m
+++ synaptic_gating.m
@@ -7,7 +7,7 @@ beta_s = 0.01;
 beta_x = 0.5;
 
 dx(1) = alpha_s * x(2) * (1-x(1)) - beta_s * x(1);
-dx(2) = alpha_x / 1 + exp(v_s_pre/2) * (1-x(1)) - beta_x * x(1)
+dx(2) = (alpha_x / 1 + exp(-v_s_pre/2)) * (1-x(2)) - beta_x * x(2);
 
 
 end
